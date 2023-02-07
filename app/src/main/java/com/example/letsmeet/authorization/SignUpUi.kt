fff70b0a7e528f27f0d3faa96bb65dedbb5e5907
Lee-GS <leegs4831@naver.com>
2023-02-07 20:45:10 +0900

    feat: Add authorization (#1)

---
+++ app/src/main/java/com/example/letsmeet/authorization/SignUpUi.kt
@@ -0,0 +1,43 @@
+package com.example.letsmeet.authorization
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.letsmeet.authorization.ui.theme.ui.theme.LetsMeetTheme
+
+class SignUpUi : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LetsMeetTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting2("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview2() {
+    LetsMeetTheme {
+        Greeting2("Android")
+    }
+}
