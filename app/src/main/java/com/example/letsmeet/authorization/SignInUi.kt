face84e6c31cf5c1dfe0ac0255f326a961f272e6
Lee-GS <leegs4831@naver.com>
2023-02-13 03:02:25 +0900

    feat: fix fun signIn() (#6)

--- app/src/main/java/com/example/letsmeet/authorization/SignInUi.kt
+++ app/src/main/java/com/example/letsmeet/authorization/SignInUi.kt
@@ -6,14 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement.Absolute.Center
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.letsmeet.authorization.ui.theme.LetsMeetTheme
+import java.net.PasswordAuthentication
 
 class SignInUi : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,21 +36,28 @@ class SignInUi : ComponentActivity() {
 
 @Composable
 fun signIn(modifier: Modifier) {
-
+    var email = rememberSaveable{
+        mutableStateOf("")
+    }
+    var pw = rememberSaveable {
+        mutableStateOf("")
+    }
     Column(
         Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        TextField(value = "",
-            onValueChange = {},
-            placeholder = { Text(text = "Email") },
+        TextField(value = email.value,
+            onValueChange = {emailValue -> email.value = emailValue},
+            label = {Text("Email")},
             modifier = modifier
                 .fillMaxWidth()
         )
-        TextField(value = "",
-            onValueChange = {},
-            placeholder = { Text(text = "PW") },
+        TextField(value = pw.value,
+            onValueChange = {pwValue -> pw.value = pwValue},
+            label = { Text("PW") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
             modifier = modifier
                 .fillMaxWidth()
 
