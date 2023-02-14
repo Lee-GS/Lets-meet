bd2e58c1053bdb47d5725e0f4b7c44992c7caaf4
Lee-GS <leegs4831@naver.com>
2023-02-15 02:01:40 +0900

    feat: fix fun checkPW (#9)

--- app/src/main/java/com/example/letsmeet/authorization/SignUpUi.kt
+++ app/src/main/java/com/example/letsmeet/authorization/SignUpUi.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import com.example.letsmeet.authorization.ui.theme.ui.theme.LetsMeetTheme
 
@@ -25,35 +30,51 @@ class SignUpUi : ComponentActivity() {
 
 @Composable
 fun signUp(modifier: Modifier) {
+    var email = rememberSaveable{
+        mutableStateOf("")
+    }
+    var pw = rememberSaveable {
+        mutableStateOf("")
+    }
+    var name = rememberSaveable {
+        mutableStateOf("")
+    }
+    var pw_check = rememberSaveable {
+        mutableStateOf("")
+    }
 
     Column(
         Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        TextField(value = "",
-            onValueChange = {},
+        TextField(value = name.value,
+            onValueChange = {nameValue -> name.value = nameValue},
             label = { Text("이름") },
             modifier = modifier
                 .fillMaxWidth()
         )
-        TextField(value = "",
-            onValueChange = {},
+        TextField(value = email.value,
+            onValueChange = {emailValue -> email.value = emailValue},
             label = { Text("이메일") },
             modifier = modifier
                 .fillMaxWidth()
 
         )
-        TextField(value = "",
-            onValueChange = {},
+        TextField(value = pw.value,
+            onValueChange = {pwValue -> pw.value = pwValue},
             label = { Text("비밀번호") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
             modifier = modifier
                 .fillMaxWidth()
 
         )
-        TextField(value = "",
-            onValueChange = {},
+        TextField(value = pw_check.value,
+            onValueChange = {pw_checkValue -> pw_check.value = pw_checkValue},
             label = { Text("비밀먼호 확인") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
             modifier = modifier
                 .fillMaxWidth()
 
