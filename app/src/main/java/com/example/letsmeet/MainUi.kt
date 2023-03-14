def42932fb2a8b713c3e961a2c47b49e3dfb829b
Lee-GS <leegs4831@naver.com>
2023-03-14 22:11:57 +0900

    feat: fix fun TimeLine (#17)

--- app/src/main/java/com/example/letsmeet/MainUi.kt
+++ app/src/main/java/com/example/letsmeet/MainUi.kt
@@ -1,9 +1,6 @@
 package com.example.letsmeet
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,6 +12,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import java.sql.Time
 
 @Composable
@@ -34,16 +32,28 @@ fun TimeLine(modifier: Modifier){
         mutableStateOf("")
     }
     Row(
-        Modifier.fillMaxSize(),
-        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
-    ) {
+        horizontalArrangement = Arrangement.SpaceAround,
+        )
+        {
         TextField(
             value = time.value,
             onValueChange = { timeValue -> time.value = timeValue},
-            label = { Text("이름") },
-            modifier = modifier
-                .fillMaxWidth()
+            label = { Text("시간") },
+            modifier = modifier.width(100.dp)
+        )
+        TextField(
+            value = place.value,
+            onValueChange = { placeValue -> place.value = placeValue},
+            label = { Text("장소") },
+            modifier = modifier.width(100.dp)
+        )
+        TextField(
+            value = plan.value,
+            onValueChange = { planValue -> plan.value = planValue},
+            label = { Text("계획") },
+            modifier = modifier.width(100.dp)
         )
     }
 }
@@ -58,6 +68,6 @@ fun MainUiPreview(){
 @Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
 @Composable
 fun TimeLinePreview(){
-    TimeLine(Modifier)
+    TimeLine(modifier = Modifier.padding(8.dp))
 }
 
