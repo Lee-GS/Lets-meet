3bc2792e2016abd4f73ae0b7d615f2e085f3010e
Lee-GS <leegs4831@naver.com>
2023-03-14 16:41:38 +0900

    feat: Add fun TimeLine (#16)

--- app/src/main/java/com/example/letsmeet/MainUi.kt
+++ app/src/main/java/com/example/letsmeet/MainUi.kt
@@ -1,23 +1,63 @@
 package com.example.letsmeet
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import java.sql.Time
 
 @Composable
 fun MainUi(){
-    Scaffold(bottomBar = {
-        BottomAppBar() {
-            
-        }
-    }) {
 
+}
+
+@Composable
+fun TimeLine(modifier: Modifier){
+    var time = rememberSaveable {
+        mutableStateOf("")
+    }
+    var place = rememberSaveable {
+        mutableStateOf("")
+    }
+    var plan = rememberSaveable {
+        mutableStateOf("")
+    }
+    Row(
+        Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        TextField(
+            value = time.value,
+            onValueChange = { timeValue -> time.value = timeValue},
+            label = { Text("이름") },
+            modifier = modifier
+                .fillMaxWidth()
+        )
     }
 }
+
 
 @Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
 @Composable
 fun MainUiPreview(){
     MainUi()
 }
+
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Composable
+fun TimeLinePreview(){
+    TimeLine(Modifier)
+}
+
