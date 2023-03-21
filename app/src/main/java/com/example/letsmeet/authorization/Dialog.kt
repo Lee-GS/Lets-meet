51b266d19c7cb4981e427a20a7cf58aae6e07f40
Lee-GS <leegs4831@naver.com>
2023-03-21 14:52:12 +0900

    feat: Add popBackStack() (#25)

--- app/src/main/java/com/example/letsmeet/authorization/Dialog.kt
+++ app/src/main/java/com/example/letsmeet/authorization/Dialog.kt
@@ -6,20 +6,15 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.letsmeet.Screen
 
 
 @Composable
@@ -27,7 +22,6 @@ fun dialogContent(navController: NavController){
     Column(
         horizontalAlignment = Alignment.End,
         verticalArrangement = Arrangement.Bottom,
-
     ){
         Spacer(
             modifier = Modifier
@@ -47,7 +41,9 @@ fun dialogContent(navController: NavController){
                 .fillMaxWidth()
         )
         Button(
-            onClick = {navController.navigate(Screen.SignInScreen.route)},
+            onClick = {
+                navController.popBackStack("signin_screen",inclusive = false)
+                      },
             modifier = Modifier.padding(5.dp)
 
         ) {
