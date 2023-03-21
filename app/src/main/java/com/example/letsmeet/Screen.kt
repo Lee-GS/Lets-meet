58f1912cc14a079d52a4eb426158ab39c9aedd5a
Lee-GS <leegs4831@naver.com>
2023-03-21 17:13:59 +0900

    feat: Add MainScreen  (#28)

--- app/src/main/java/com/example/letsmeet/Screen.kt
+++ app/src/main/java/com/example/letsmeet/Screen.kt
@@ -3,5 +3,7 @@ package com.example.letsmeet
 sealed class Screen(val route: String){
     object SignInScreen: Screen("signin_screen")
     object SignUpScreen: Screen("signup_screen")
-    object dialogScreen: Screen("dialog_screen")
+    object DialogScreen: Screen("dialog_screen")
+    object MainScreen: Screen("Main_screen")
+
 }
