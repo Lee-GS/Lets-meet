a4d023ae317c99e75a61298b594afb89bf3956f5
Lee-GS <leegs4831@naver.com>
2023-03-27 00:17:00 +0900

    feat: Add firestore (#42)

--- app/src/main/java/com/example/letsmeet/authorization/AuthfFireBase.kt
+++ app/src/main/java/com/example/letsmeet/authorization/AuthfFireBase.kt
@@ -3,11 +3,13 @@ package com.example.letsmeet.authorization
 import androidx.multidex.MultiDexApplication
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 class AuthFireBase : MultiDexApplication() {
     companion object{
         lateinit var auth: FirebaseAuth
+        lateinit var firestore : FirebaseFirestore
         var email: String? = null
         fun checkAuth(): Boolean {
             val currentUser = auth.currentUser
@@ -23,6 +25,7 @@ class AuthFireBase : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         auth = Firebase.auth
+        firestore = FirebaseFirestore.getInstance()
     }
 
 }
