730dc1a2ac01663237bc48f47aa32540837e6a2a
Lee-GS <leegs4831@naver.com>
2023-04-03 19:15:59 +0900

    feat: addFriend 함수를 MainActivity의 onCreate안으로 옮김 (#51)

--- app/src/main/java/com/example/letsmeet/MainActivity.kt
+++ app/src/main/java/com/example/letsmeet/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.letsmeet.authorization.signIn
+import com.example.letsmeet.navigationDrawer.addFriend
 import com.example.letsmeet.ui.theme.LetsMeetTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,6 +26,7 @@ class MainActivity : ComponentActivity() {
             LetsMeetTheme {
                 val navController : NavHostController = rememberNavController()
                 Navigation(navController = navController)
+                addFriend()
             }
         }
     }
