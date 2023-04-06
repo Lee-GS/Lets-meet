a51eac31881e8b7b6270af70e53aa7a7250dd12e
Lee-GS <leegs4831@naver.com>
2023-04-07 01:55:38 +0900

    feat: UserDTO에 friendList 추가 (#53)

--- app/src/main/java/com/example/letsmeet/authorization/UserDTO.kt
+++ app/src/main/java/com/example/letsmeet/authorization/UserDTO.kt
@@ -3,5 +3,6 @@ package com.example.letsmeet.authorization
 data class UserDTO(
     val name : String = " ",
     val email : String = " ",
-    val password : String = " "
+    val password : String = " ",
+    val friendList : MutableList<String>
 )
