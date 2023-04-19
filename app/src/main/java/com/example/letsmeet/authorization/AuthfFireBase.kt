4585fab7f3c621eb88dac390e2f5472fdb4c957b
Lee-GS <leegs4831@naver.com>
2023-04-19 20:56:12 +0900

    feat: lateinit var 오류 해결 (#68)

--- app/src/main/java/com/example/letsmeet/authorization/AuthfFireBase.kt
+++ app/src/main/java/com/example/letsmeet/authorization/AuthfFireBase.kt
@@ -7,11 +7,20 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 class AuthFireBase : MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        auth = Firebase.auth
+        firestore = FirebaseFirestore.getInstance()
+    }
+
     companion object{
         lateinit var auth: FirebaseAuth
         lateinit var firestore : FirebaseFirestore
         var email: String? = null
         fun checkAuth(): Boolean {
+            if (!::auth.isInitialized) {
+                auth = Firebase.auth
+            }
             val currentUser = auth.currentUser
             return currentUser?.let {
                 email = currentUser.email
@@ -22,10 +31,5 @@ class AuthFireBase : MultiDexApplication() {
         }
     }
 
-    override fun onCreate() {
-        super.onCreate()
-        auth = Firebase.auth
-        firestore = FirebaseFirestore.getInstance()
-    }
-
 }
+
