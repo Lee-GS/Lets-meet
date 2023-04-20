d6cb57dc6d888157d5c6fbb198adc06532251645
Lee-GS <leegs4831@naver.com>
2023-04-20 23:52:53 +0900

    feat: rebuildFriendData 추가 (#69)

--- app/src/main/java/com/example/letsmeet/navigationDrawer/FriendData.kt
+++ app/src/main/java/com/example/letsmeet/navigationDrawer/FriendData.kt
@@ -1,8 +1,36 @@
 package com.example.letsmeet.navigationDrawer
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateListOf
+import com.example.letsmeet.mainScreen.currentEmail
+import com.google.firebase.firestore.FieldValue
 
-class FriendData {
-    val friendList = mutableStateListOf<String>()
-    val friendrequest = mutableStateListOf<String>()
+
+fun rebuildFriendData(check : Boolean) {
+    val friendList = mutableListOf<String>()
+    val friendRequest = mutableListOf<String>()
+
+    if (currentEmail != null) {
+        db.collection("users").document(currentEmail).get().addOnSuccessListener { document ->
+            if (document != null) {
+                friendRequest.add(document.get("friendrequest").toString())
+                Log.d("Succcess", friendRequest.toString())
+                if (check) {
+                    for (i in 0 until friendRequest.size){
+                        friendList.add(friendRequest[i])
+                    }
+                    friendRequest.clear()
+                }
+                db.collection("users").document(currentEmail).update("friendrequest",friendRequest).addOnSuccessListener {
+                    Log.d("SUCCESS","친구 승인 목록 삭제 성공")
+                }
+                db.collection("users").document(currentEmail).update("friendlist",friendList).addOnSuccessListener {
+                    Log.d("SUCCESS","친구 목록 업데이트 성공")
+                }
+
+            }
+        }
+        friends = friendList.toMutableList()
+        Log.d("친구 목록:", friends.toString())
+    }
 }
