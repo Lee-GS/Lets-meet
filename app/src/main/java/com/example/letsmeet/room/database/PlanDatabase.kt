14b7fffb89255b6ac70cc07d4a3c93069cd733b1
Lee-GS <leegs4831@naver.com>
2023-05-29 03:11:53 +0900

    feat: PlanDatabase build 추가  (#92)

--- app/src/main/java/com/example/letsmeet/room/database/PlanDatabase.kt
+++ app/src/main/java/com/example/letsmeet/room/database/PlanDatabase.kt
@@ -1,12 +1,33 @@
 package com.example.letsmeet.room.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.example.letsmeet.room.dao.ContentDao
 import com.example.letsmeet.room.dao.PlanDao
 import com.example.letsmeet.room.entity.ContentData
 import com.example.letsmeet.room.entity.PlanData
 
 @Database(entities = [PlanData::class], version = 1)
-abstract class PlanDatabase {
+abstract class PlanDatabase: RoomDatabase() {
     abstract fun planDao() : PlanDao
+
+    companion object{
+        private var instance: PlanDatabase? = null
+
+        @Synchronized
+        fun getInstance(context: Context): PlanDatabase?{
+            if (instance == null){
+                synchronized(PlanDatabase::class){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        PlanDatabase::class.java,
+                        "plan.db"
+                    ).build()
+                }
+            }
+            return instance
+        }
+    }
 }
