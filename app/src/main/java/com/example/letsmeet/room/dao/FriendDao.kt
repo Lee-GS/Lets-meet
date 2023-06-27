4bb0c10559a9bd44e08d7fca738265efebc833b7
Lee-GS <leegs4831@naver.com>
2023-06-27 20:44:51 +0900

    chore: dao 매개변수 변경  (#106)

--- app/src/main/java/com/example/letsmeet/room/dao/FriendDao.kt
+++ app/src/main/java/com/example/letsmeet/room/dao/FriendDao.kt
@@ -7,11 +7,11 @@ import com.example.letsmeet.room.entity.PlanData
 @Dao
 interface FriendDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertFriend(vararg name: String)
+    suspend fun insertFriend(friendData: FriendData)
     @Update
-    suspend fun updateFriend(vararg name: String)
+    suspend fun updateFriend(friendData: FriendData)
     @Delete
-    suspend fun deleteFriend(vararg name: String)
+    suspend fun deleteFriend(friendData: FriendData)
     @Query("SELECT * FROM friends")
-    suspend fun getAll(): ArrayList<FriendData>
+    suspend fun getAll(): List<FriendData>
 }
