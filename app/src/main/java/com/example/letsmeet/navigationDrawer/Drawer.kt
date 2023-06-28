574629f155938f7b51068d771c1d619187dd6b46
Lee-GS <leegs4831@naver.com>
2023-06-29 01:43:37 +0900

    mod : LazyColumn 친추 목록 나오게 수정 (#112)

--- app/src/main/java/com/example/letsmeet/navigationDrawer/Drawer.kt
+++ app/src/main/java/com/example/letsmeet/navigationDrawer/Drawer.kt
@@ -7,30 +7,34 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.letsmeet.MyApplication
+import com.example.letsmeet.MainActivity
 import com.example.letsmeet.R
 import com.example.letsmeet.navigationDrawer.FriendData.Companion.friends
 import com.example.letsmeet.room.database.FriendDatabase
+import com.example.letsmeet.room.entity.FriendData
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 
 fun addFriend(){
-    val db = FriendDatabase.getInstance(MyApplication.ApplicationContext())
+    val db = FriendDatabase.getInstance(MainActivity.applicationContext())
     CoroutineScope(Dispatchers.IO).launch {
-        friends.addAll(db!!.friendDao().getAll())
-        Log.d("친구목록",friends.toString())
+        friends = db!!.friendDao().getAll() as ArrayList<FriendData>
+        Log.d("친구 목록","$friends")
     }
 }
 
 
 @Composable
 fun FriendList(modifier: Modifier) {
+    Log.d("drawer","flag")
+    addFriend()
     Column(
         modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
@@ -43,7 +47,7 @@ fun FriendList(modifier: Modifier) {
         )
         {
             items(friends) {
-                it
+                item: FriendData -> Text(text = item.name )
             }
         }
     }
