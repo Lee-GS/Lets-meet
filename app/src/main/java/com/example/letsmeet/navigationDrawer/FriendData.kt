4a35ab502f2a661599fd637c1a46605b1e14996a
Lee-GS <leegs4831@naver.com>
2023-06-30 02:50:17 +0900

    mod : db작업 스레드로 이동 및 자잘한 log.d 삭제 (#115)

--- app/src/main/java/com/example/letsmeet/navigationDrawer/FriendData.kt
+++ app/src/main/java/com/example/letsmeet/navigationDrawer/FriendData.kt
@@ -29,10 +29,8 @@ fun rebuildFriendData(check : Boolean, name : String) {
     if (AuthFireBase.email != null) {
         db.collection("users").document(AuthFireBase.email!!).get().addOnSuccessListener { document ->
             if (document != null) {
-                Log.d("친구요청 한 사람", "${document.get("friendrequest")}")
                 friendRequest= document.get("friendrequest") as ArrayList<String>
-                Log.d("친구요청 목록", "${friendRequest}")
-                Log.d("친구요청 목록 사이즈","${friendRequest.size}")
+                Log.d("친구요청 목록", friendRequest.toString())
                 if (check && friendRequest.isNotEmpty()) {
                     for (i in 0 until friendRequest.size){
                         CoroutineScope(Dispatchers.IO).launch{
@@ -41,25 +39,23 @@ fun rebuildFriendData(check : Boolean, name : String) {
                                 FriendData(friendRequest[i])
                             )
                             withContext(Dispatchers.Main){
-                                friendRequest.clear()
-                                Log.d("flag",friendRequest.toString())
+                                for (i in 0 until friends.size) {
+                                    db.collection("users").document(AuthFireBase.email!!)
+                                        .update("friendlist", FieldValue.arrayUnion(friends[i].name))
+                                        .addOnSuccessListener {
+                                            Log.d("친구 목록 업데이트 성공", friends.toString())
+                                        }
+                                }
+                                db.collection("users").document(name).update("friendlist", FieldValue.arrayUnion(AuthFireBase.email)).addOnSuccessListener {
+                                    Log.d("SUCCESS","요청한 유저의 친구 목록에 현유저 추가 성공")
+                                }
+                                db.collection("users").document(AuthFireBase.email!!).update("friendrequest",FieldValue.arrayRemove(name)).addOnSuccessListener {
+                                    Log.d("SUCCESS","친구 승인 목록 삭제 성공")
+                                }
                             }
                         }
                     }
                 }
-                db.collection("users").document(name).update("friendlist", FieldValue.arrayUnion("${AuthFireBase.email}")).addOnSuccessListener {
-                    Log.d("SUCCESS","요청한 유저의 친구 목록에 현유저 추가 성공")
-                }
-                db.collection("users").document(AuthFireBase.email!!).update("friendrequest",friendRequest).addOnSuccessListener {
-                    Log.d("SUCCESS","친구 승인 목록 삭제 성공")
-                }
-                for (i in 0..friends.size) {
-                    db.collection("users").document(AuthFireBase.email!!)
-                        .update("friendlist", FieldValue.arrayUnion(friends[i].name)).addOnSuccessListener {
-                        Log.d("친구 목록 업데이트 성공", friends.toString())
-                    }
-                }
-                Log.d("친구 목록", friends.toString())
             }
         }
     }
