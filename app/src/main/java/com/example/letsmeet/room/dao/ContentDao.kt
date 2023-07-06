bc600f3dad8363ad6983956e49f066e47a9a67d8
Lee-GS <leegs4831@naver.com>
2023-07-07 01:52:50 +0900

    add : insertDate,insertPlan  (#128)

--- app/src/main/java/com/example/letsmeet/room/dao/ContentDao.kt
+++ app/src/main/java/com/example/letsmeet/room/dao/ContentDao.kt
@@ -6,9 +6,10 @@ import com.example.letsmeet.room.entity.ContentData
 @Dao
 interface ContentDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertContents(contentData: ContentData)
+    suspend fun insertDate(date : ContentData)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPlan(time : ContentData, plan : ContentData)
     @Update
-
     suspend fun updateContents(contentData: ContentData)
     @Delete
     suspend fun deleteContents(contentData: ContentData)
