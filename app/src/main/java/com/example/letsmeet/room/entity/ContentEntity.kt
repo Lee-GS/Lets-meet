dd945e3b0eab028fa346276274f0ddb10e013a3f
Lee-GS <leegs4831@naver.com>
2023-07-06 23:04:28 +0900

    del : place (#123)

--- app/src/main/java/com/example/letsmeet/room/entity/ContentEntity.kt
+++ app/src/main/java/com/example/letsmeet/room/entity/ContentEntity.kt
@@ -8,6 +8,5 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "contents")
 data class ContentData(
     @PrimaryKey val time: String = " ",
-    @ColumnInfo val place: String = " ",
     @ColumnInfo val plan: String = " "
 )
