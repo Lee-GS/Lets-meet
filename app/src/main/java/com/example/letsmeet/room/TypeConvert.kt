120d0f4dc8c760e4a425e808e611295e1464f1b9
Lee-GS <leegs4831@naver.com>
2023-07-11 02:26:00 +0900

    add : class TypeConvert (#131)

---
+++ app/src/main/java/com/example/letsmeet/room/TypeConvert.kt
@@ -0,0 +1,16 @@
+package com.example.letsmeet.room
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+
+class TypeConvert {
+    @TypeConverter
+    fun listToJson(value: List<String>?): String? {
+        return Gson().toJson(value)
+    }
+
+    @TypeConverter
+    fun jsonToList(value: String): List<String>? {
+        return Gson().fromJson(value,Array<String>::class.java)?.toList()
+    }
+}
