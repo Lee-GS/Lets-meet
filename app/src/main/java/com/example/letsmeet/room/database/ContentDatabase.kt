71b7c711b5b55ecc1dae0f3ef3d155fd2972b420
Lee-GS <leegs4831@naver.com>
2023-07-11 02:27:09 +0900

    add : TypeConverters (#133)

--- app/src/main/java/com/example/letsmeet/room/database/ContentDatabase.kt
+++ app/src/main/java/com/example/letsmeet/room/database/ContentDatabase.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.letsmeet.room.TypeConvert
 import com.example.letsmeet.room.dao.ContentDao
 import com.example.letsmeet.room.entity.ContentData
 
 @Database(entities = [ContentData::class], version = 1)
+@TypeConverters(TypeConvert::class)
 abstract class ContentDatabase : RoomDatabase(){
     abstract fun contentDao() : ContentDao
 
