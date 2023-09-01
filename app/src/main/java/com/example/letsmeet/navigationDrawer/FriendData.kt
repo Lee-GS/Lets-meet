55e16a8d9773150b0e7ffb39960cbad2550e70e5
Lee-GS <leegs4831@naver.com>
2023-09-01 16:37:52 +0900

    mod : 기능 함수화 (#149)

--- app/src/main/java/com/example/letsmeet/navigationDrawer/FriendData.kt
+++ app/src/main/java/com/example/letsmeet/navigationDrawer/FriendData.kt
@@ -1,16 +1,13 @@
 package com.example.letsmeet.navigationDrawer
 
 import android.util.Log
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import com.example.letsmeet.MainActivity
 import com.example.letsmeet.authorization.AuthFireBase
 import com.example.letsmeet.navigationDrawer.FriendData.friends
 import com.example.letsmeet.room.database.FriendDatabase
 import com.example.letsmeet.room.entity.FriendData
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FieldValue
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -23,7 +20,7 @@ object FriendData {
 
 
 fun rebuildFriendData(check: Boolean, name: String) {
-    val db = AuthFireBase.firestore
+    val db = AuthFireBase.firestore.collection("users")
     if (AuthFireBase.email != null) {
         if (check) {
             friends.add(name)
@@ -34,23 +31,35 @@ fun rebuildFriendData(check: Boolean, name: String) {
                         FriendData(name)
                     )
             }
-            db.collection("users").document(AuthFireBase.email!!)
-                .update("friendlist", FieldValue.arrayUnion(name))
-                .addOnSuccessListener {
-                    Log.d("친구 목록 업데이트 성공", name + "추가")
-                }
-            db.collection("users").document(name)
-                .update("friendlist", FieldValue.arrayUnion(AuthFireBase.email))
-                .addOnSuccessListener {
-                    Log.d("SUCCESS", "요청한 유저의 친구 목록에 현유저 추가 성공")
-                }
+            insertFriend(AuthFireBase.email!!,name,db)
+            insertRequestFriend(name,AuthFireBase.email!!,db)
         }
-        db.collection("users").document(AuthFireBase.email!!)
-            .update("friendrequest", FieldValue.arrayRemove(name))
-            .addOnSuccessListener {
-                Log.d("SUCCESS", "친구 승인 목록 삭제 성공")
-            }
+        removeFriend(AuthFireBase.email!!,name,db)
     }
 }
+fun insertFriend(name : String, friend : String, path : CollectionReference ){
+    path.document(name)
+        .update("friendlist", FieldValue.arrayUnion(friend))
+        .addOnSuccessListener {
+            Log.d("친구 목록 업데이트 성공", name + "추가")
+        }
+}
+
+fun insertRequestFriend(name : String, friend : String, path : CollectionReference ){
+    path.document(name)
+        .update("friendlist", FieldValue.arrayUnion(friend))
+        .addOnSuccessListener {
+            Log.d("SUCCESS", "요청한 유저의 친구 목록에 현유저 추가 성공")
+        }
+}
+
+fun removeFriend(name : String, friend : String, path : CollectionReference ){
+    path.document(name)
+        .update("friendlist", FieldValue.arrayRemove(friend))
+        .addOnSuccessListener {
+            Log.d("친구 목록 업데이트 성공", name + "추가")
+        }
+}
+
 
 
